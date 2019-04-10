211101cc1f5a2ed0167a724cbfb16552f037438f
Andrey <artist_hire@ukr.net>
2019-04-10 13:36:08 +0300

    Выполнена стилизация шапки сайта

--- source/js/show_menu.js
+++ source/js/show_menu.js
@@ -4,15 +4,9 @@ var toggle_btn = document.querySelector('.js-menu-toggle-btn');
 
 if (toggle_btn) {
 
-  var screen_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-  var screen_md = 768;
-
   //установка исходного состояния главного меню (закрыто)
   toggle_btn.classList.remove('menu-toggle-btn--open');
   toggle_btn.classList.add('menu-toggle-btn--close');
-
-  if (toggle_btn.hasAttribute('aria-label'))
-    toggle_btn.setAttribute('aria-label', 'Открыть меню');
 
 
   //обработка отрытия меню
@@ -23,16 +17,10 @@ if (toggle_btn) {
     if(this.classList.contains('menu-toggle-btn--open')) {
       this.classList.remove('menu-toggle-btn--open');
       this.classList.add('menu-toggle-btn--close');
-
-      if (this.hasAttribute('aria-label'))
-        this.setAttribute('aria-label', 'Открыть меню');
     }
     else {
       this.classList.add('menu-toggle-btn--open');
       this.classList.remove('menu-toggle-btn--close');
-
-      if (this.hasAttribute('aria-label'))
-        this.setAttribute('aria-label', 'Закрыть меню');
     }
   });
 }
